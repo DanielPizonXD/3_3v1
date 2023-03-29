007291dde0a177a883b689846fdbc619284da8c3
DanielPizonXD <126687080+DanielPizonXD@users.noreply.github.com>
2023-03-29 19:24:14 +0000

    Initial commit

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
+import java.util.Random;
+
 class Main {
   public static void main(String[] args) {
 int [][] array=new int[10][10];
     Random rand = new Random();
     for(int i=0;i<10;i++){
-      
+      for (int j = 0; j<10;j++){
+        array[i][j]=rand.nextInt(101)-50;
+        System.out.print(array[i][j]+ "\t");
+      }
+      System.out.println();
     }
   }
 }
